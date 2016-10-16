13bdbd9683ecb4bc8995a27dea6a16fd285fbdcb
Denes Pal <denes.pal@lab.coop>
2016-10-16 23:47:30 +0200

    feat(redux): Implement initial state file
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

---
+++ src/initial-state.js
@@ -0,0 +1,19 @@
+export default {
+  form: {
+    errors: {
+      name: false,
+      email: false,
+      occupation: false,
+      birthday: false
+    },
+    values: {
+      name: false,
+      email: false,
+      occupation: false,
+      birthday: false
+    }
+  },
+  api: {
+    error: false
+  }
+};
