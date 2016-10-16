7ac2e6979b6e9aaf0accc5b892197779165814d7
Denes Pal <denes.pal@lab.coop>
2016-10-17 00:02:13 +0200

    feat(App): Add redux, create store
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 injectTapEventPlugin();
 
+import initialState from './initial-state';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, initialState, applyMiddleware(thunk));
+
 const App = ({title}) => (
   <MuiThemeProvider>
+    <Provider store={store}>
       <div className='container'>
       </div>
+    </Provider>
   </MuiThemeProvider>
 );
 
