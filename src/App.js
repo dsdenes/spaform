11f64422b709b9e3b28058c753586932941b2a28
Denes Pal <denes.pal@lab.coop>
2016-10-17 00:21:09 +0200

    feat(App): Add Head component to App
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ import thunk from 'redux-thunk';
 
 const store = createStore(reducers, initialState, applyMiddleware(thunk));
 
+import Head from './componets/head/index'
 const App = ({title}) => (
   <MuiThemeProvider>
     <Provider store={store}>
       <div className='container'>
+        <Head title={title} />
       </div>
     </Provider>
   </MuiThemeProvider>
