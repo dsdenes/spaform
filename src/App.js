4e4f27ea44751d197cddb3fa1e0d088f7acced9b
Denes Pal <denes.pal@lab.coop>
2016-10-16 23:44:37 +0200

    feat(App): Add material-ui container provider
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 injectTapEventPlugin();
 
 const App = ({title}) => (
+  <MuiThemeProvider>
       <div className='container'>
       </div>
+  </MuiThemeProvider>
 );
 
 App.defaultProps = {
