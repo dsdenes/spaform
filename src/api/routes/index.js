a26d722d58cf8e0728fedaf90469a3e8eb014490
Denes Pal <denes.pal@lab.coop>
2016-11-05 16:32:46 +0100

    feat(api): Implement form saving route
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const wrapMiddleware = require('../lib/wrap-middleware');
 const restResponse = require('../lib/rest-response');
+const handleValidationError = require('../lib/handle-validation-error');
+const container = require('../../services');
+const formService = container.form;
+
 function getOccupations() {
   return [
     'Audiologist',
@@ -24,4 +28,11 @@ router.get('/occupations',
   wrapMiddleware(getOccupations),
   restResponse
 );
+
+router.post('/form',
+  wrapMiddleware(formService.save),
+  restResponse,
+  handleValidationError
+);
+
 module.exports = router;
