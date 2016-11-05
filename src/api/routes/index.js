6c1d281ec13e7d0701f49f09ab67ff92b890f853
Denes Pal <denes.pal@lab.coop>
2016-11-05 16:32:19 +0100

    feat(api): Implement occupations route
    Gives back a list of available occupations.
    
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const wrapMiddleware = require('../lib/wrap-middleware');
+const restResponse = require('../lib/rest-response');
+function getOccupations() {
+  return [
+    'Audiologist',
+    'Speech Pathologist',
+    'General Practitioner',
+    'Anaesthetist',
+    'Specialist Physician',
+    'Cardiologist',
+    'Clinical Haematologist',
+    'Medical Oncologist',
+    'Endocrinologist',
+    'Gastroenterologist',
+    'Intensive Care Specialist',
+    'Neurologist'
+  ];
+}
+
+router.get('/occupations',
+  wrapMiddleware(getOccupations),
+  restResponse
+);
 module.exports = router;
