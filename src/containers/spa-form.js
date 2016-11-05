0f31efe099164c0d0e19d4b61a2ea883f6011dd1
Denes Pal <denes.pal@lab.coop>
2016-11-05 15:51:35 +0100

    feat(App): Trigger field validation on change
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- src/containers/spa-form.js
+++ src/containers/spa-form.js
@@ -8,9 +8,11 @@ import Button from '../componets/button/index';
 import './spa-form.css';
 
 class SPAForm extends Component {
+import { inputChanged } from '../actions/form/input';
   render() {
     const {
       errors,
+      inputChanged,
     } = this.props;
 
     return (<form
@@ -20,12 +22,14 @@ class SPAForm extends Component {
         name="name"
         hintText="Name"
         errorText={errors.name}
+        onChange={event => inputChanged('name', event.target.value)}
       />
       <Input
         type="email"
         name="email"
         hintText="E-mail"
         errorText={errors.email}
+        onChange={event => inputChanged('email', event.target.value)}
       />
       <OccupationInput
         inputChanged={inputChanged}
@@ -34,6 +38,7 @@ class SPAForm extends Component {
         name="birthday"
         hintText="Birthday"
         errorText={errors.birthday}
+        onChange={(event, date) => inputChanged('birthday', date)}
       />
       <Button
         text="Send form"
@@ -47,6 +52,13 @@ const mapStateToProps = (state, ownProps) => ({
   errors: state.form.errors,
 });
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    inputChanged: (name, value) => dispatch(inputChanged(name, value)),
+  }
+};
+
 export default connect(
   mapStateToProps,
+  mapDispatchToProps
 )(SPAForm);
