af3415a505b7e79442abbe03244f1a56e7ed20fd
Denes Pal <denes.pal@lab.coop>
2016-11-05 15:29:34 +0100

    feat(reducers): Implement API related reducers
    Can be useful later if we want to catch API request events
    
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

---
+++ src/reducers/api/index.js
@@ -0,0 +1,5 @@
+import initialState from '../../initial-state';
+
+export default (state = initialState.api, action) => {
+  return state;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import form from './form/';
+import api from './api/';
 import notification from './notification/';
 
 export default combineReducers({
   form,
+  api,
   notification
 });
